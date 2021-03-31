5d995ba15b0023982626c5a9071b049d83e7e871
shelleyHLX <2501967525@qq.com>
2021-03-31 17:54:37 +0800

    first

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 // 创建一个小程序
 App({
-  // 生命周期函数指的是某一时刻会自动执行的函数
-  // 在小程序启动的时候，自动执行的函数
-  onLaunch(options) {
-    console.log(options);
-    // {path: "pages/index/index", query: {…}, scene: 1001, shareTicket: undefined, referrerInfo: {…}}
-    // path: "pages/index/index"
-    // query: {}
-    // referrerInfo: {}
-    // scene: 1001
-    // shareTicket: undefined
-    // __proto__: Object
-  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,37 @@
-// index.js
-// 获取应用实例
-const app = getApp()
+// pages/index.js
+Page({
 
-Page({})
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    showIcon: true,
+    swiperList: [],
+    course: []
+  },
+  onLoad(){
+    wx.request({
+      url: 'https://www.fastmock.site/mock/2d5d21a7be5236e088689ab6ce04b921/weixin/api/getData',
+      success: (res) => {
+        const {data:{data}} = res;
+        const{ swiperList, course } = data;
+        // console.log(data);
+        this.setData({
+          swiperList,
+          course
+        })
+      }
+    })
+  },
+  handleInputChange(e){
+    const value = e.detail.value;
+    this.setData({showIcon: value?false:true})
+    // if(value){
+    //   this.setData({showIcon: false})
+    // }else{
+    //   this.setData({showIcon: true})
+    // }
+  },
+  
+
+})
